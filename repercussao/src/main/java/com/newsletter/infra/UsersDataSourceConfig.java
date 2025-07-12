ae0617de9442b9e1f7895adfef218521d8f2fcd5
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2025-07-12 17:54:43 -0300

    Infra Databases

---
+++ repercussao/src/main/java/com/newsletter/infra/UsersDataSourceConfig.java
@@ -0,0 +1,46 @@
+package com.newsletter.infra;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.util.Objects;
+
+@Configuration
+@EnableJpaRepositories(
+        basePackages = "com.newsletter.repository.user",
+        entityManagerFactoryRef = "usersEntityManagerFactory",
+        transactionManagerRef = "usersTransactionManager"
+)
+public class UsersDataSourceConfig {
+
+    @Bean(name = "usersDataSource")
+    @ConfigurationProperties(prefix = "spring.datasource.users")
+    public DataSource usersDataSource() {
+        return DataSourceBuilder.create().build();
+    }
+
+    @Bean(name = "usersEntityManagerFactory")
+    public LocalContainerEntityManagerFactoryBean usersEntityManagerFactory(
+            @Qualifier("usersDataSource") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(dataSource);
+        em.setPackagesToScan("com.newsletter.model.user"); // Pacote da entidade Users
+        em.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+        return em;
+    }
+
+    @Bean(name = "usersTransactionManager")
+    public PlatformTransactionManager usersTransactionManager(
+            @Qualifier("usersEntityManagerFactory") LocalContainerEntityManagerFactoryBean factory) {
+        return new JpaTransactionManager(Objects.requireNonNull(factory.getObject()));
+    }
+}
